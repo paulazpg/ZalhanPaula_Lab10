7bb833b089da1dea57acb9874126e6e19508f991
Paula - Georgiana ZALHAN <Paula.ZALHAN@office.ubbcluj.ro>
2020-12-15 09:13:57 +0200

    Add project files.

---
+++ ZalhanPaula_Lab10/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZalhanPaula_Lab10.Data;
+namespace ZalhanPaula_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            //inlocuire instructiune pas 15
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
